a8ce21832192dc4efa54cf4a6865d896983c2cbf
RuslanPr0g <theruslan.prog@gmail.com>
2021-01-23 22:08:31 +0300

    add token extention

---
+++ ASPNETunderthehood/Extentions/TokenExtentions.cs
@@ -0,0 +1,13 @@
+﻿using ASPNETunderthehood.Middlewares;
+using Microsoft.AspNetCore.Builder;
+
+namespace ASPNETunderthehood.Extentions
+{
+    public static class TokenExtensions
+    {
+        public static IApplicationBuilder UseToken(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<TokenMiddleware>();
+        }
+    }
+}
