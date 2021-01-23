9037b9a9adfcce2ace936a272eb18579994a6f3c
RuslanPr0g <theruslan.prog@gmail.com>
2021-01-23 22:07:08 +0300

    add token middleware

---
+++ ASPNETunderthehood/Middlewares/TokenWiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace ASPNETunderthehood.Middlewares
+{
+    public class TokenMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public TokenMiddleware(RequestDelegate next)
+        {
+            this._next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var token = context.Request.Query["token"];
+            if (token != "ruslan")
+            {
+                context.Response.StatusCode = 403;
+                await context.Response.WriteAsync("Token is invalid");
+            }
+            else
+            {
+                await _next.Invoke(context);
+            }
+        }
+    }
+}
