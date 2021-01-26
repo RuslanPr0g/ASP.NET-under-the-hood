17817d9bebc8e862def4b228bdf8f6d68377254b
RuslanPr0g <theruslan.prog@gmail.com>
2021-01-26 14:29:21 +0300

    add error handling middleware

---
+++ ASPNETunderthehood/Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETunderthehood.Middlewares
+{
+    public class ErrorHandlingMiddleware
+    {
+        private RequestDelegate _next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            await _next.Invoke(context);
+            if (context.Response.StatusCode == 403)
+            {
+                await context.Response.WriteAsync("Access Denied for you.");
+            }
+            else if (context.Response.StatusCode == 404)
+            {
+                await context.Response.WriteAsync("Not Found page.");
+            }
+        }
+    }
+}
