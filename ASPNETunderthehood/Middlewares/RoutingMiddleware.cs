3f3de931af290cea88f97ef3e898e547bac45c98
RuslanPr0g <theruslan.prog@gmail.com>
2021-01-26 14:12:23 +0300

    add routing middleware

---
+++ ASPNETunderthehood/Middlewares/RoutingMiddleware.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETunderthehood.Middlewares
+{
+    public class RoutingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public RoutingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            string path = context.Request.Path.Value.ToLower();
+            if (path == "/index")
+            {
+                await context.Response.WriteAsync("Home Page");
+            }
+            else if (path == "/about")
+            {
+                await context.Response.WriteAsync("About");
+            }
+            else
+            {
+                context.Response.StatusCode = 404;
+            }
+        }
+    }
+}
