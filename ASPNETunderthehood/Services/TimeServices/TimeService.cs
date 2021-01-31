46093acb44565a7adeda078e27699aadae5aa6d0
RuslanPr0g <theruslan.prog@gmail.com>
2021-01-31 21:33:22 +0300

    add time service

---
+++ ASPNETunderthehood/Services/TimeServices/TimeService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETunderthehood.Services.TimeServices
+{
+    public class TimeService
+    {
+        public string GetTime() => System.DateTime.Now.ToString("hh:mm:ss");
+    }
+}
