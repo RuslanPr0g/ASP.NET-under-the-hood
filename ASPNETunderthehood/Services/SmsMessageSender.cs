27ada2d1d9250292180179fdf8dcb40bf732201c
RuslanPr0g <theruslan.prog@gmail.com>
2021-01-31 21:11:01 +0300

    add message senders

---
+++ ASPNETunderthehood/Services/SmsMessageSender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETunderthehood.Services
+{
+    public class SmsMessageSender : IMessageSender
+    {
+        public string Send()
+        {
+            return "Send via sms.";
+        }
+    }
+}
