a42adb1f26b296f70acf23f55772657367365a3e
RuslanPr0g <theruslan.prog@gmail.com>
2021-01-31 21:54:24 +0300

    extention for time service

---
+++ ASPNETunderthehood/Extentions/ServiceProviderExtensions.cs
@@ -0,0 +1,13 @@
+﻿using ASPNETunderthehood.Services.TimeServices;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ASPNETunderthehood.Extentions
+{
+    public static class ServiceProviderExtensions
+    {
+        public static void AddTimeService(this IServiceCollection services)
+        {
+            services.AddTransient<TimeService>();
+        }
+    }
+}
