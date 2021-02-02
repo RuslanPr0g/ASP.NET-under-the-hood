dccc70ba7321dee3cc50baa80b97d2da20210d0e
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-02 10:58:26 +0300

    counter service to undertand DI better

---
+++ ASPNETunderthehood/Services/Counter/CounterService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETunderthehood.Services.Counter
+{
+    public class CounterService
+    {
+        protected internal ICounter Counter { get; }
+        public CounterService(ICounter counter)
+        {
+            Counter = counter;
+        }
+    }
+}
