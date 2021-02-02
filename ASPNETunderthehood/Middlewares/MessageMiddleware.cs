6437e92f1b83cb1b658a57aefc3348458b92db0f
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-02 10:35:00 +0300

    message sender middleware added

---
+++ ASPNETunderthehood/Middlewares/MessageMiddleware.cs
@@ -0,0 +1,25 @@
+﻿using ASPNETunderthehood.Services;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETunderthehood.Middlewares
+{
+    public class MessageMiddleware
+    {
+        private RequestDelegate _next;
+
+        public MessageMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, IMessageSender sender)
+        {
+            context.Response.ContentType = "text/html;charset=utf-8";
+            await context.Response.WriteAsync(sender.Send());
+        }
+    }
+}
