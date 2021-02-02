9db27c5c9a5bb38bd9d68e6bfb33381f9f56544e
RuslanPr0g <theruslan.prog@gmail.com>
2021-02-02 10:57:09 +0300

    add counter

---
+++ ASPNETunderthehood/Services/Counter/ICounter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETunderthehood.Services.Counter
+{
+    public interface ICounter
+    {
+        int Value { get; }
+    }
+}
---
+++ ASPNETunderthehood/Services/Counter/RandomCounter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETunderthehood.Services.Counter
+{
+    public class RandomCounter : ICounter
+    {
+        static readonly Random Rnd = new Random();
+
+        private readonly int _value;
+
+        public RandomCounter()
+        {
+            _value = Rnd.Next(0, 1000000);
+        }
+
+        public int Value
+        {
+            get { return _value; }
+        }
+    }
+}
