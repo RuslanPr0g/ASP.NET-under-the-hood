9bba6581344f56d5fc18e5f9064226894cfad23e
RuslanPr0g <theruslan.prog@gmail.com>
2021-04-18 10:58:31 +0300

    add home controller

---
+++ ASPNETMVCfromEMPTY/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNETMVCfromEMPTY.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
